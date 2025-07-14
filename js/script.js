8784d255e539fa3b94e8c7c0cdab5a18f71b0c08
Sushant Rahate <sushantrahate15@gmail.com>
2025-07-14 20:52:19 +0530

    update

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
   generateBtn.addEventListener('click', () => {
     const ipSegment1 = document.getElementById('ipSegment1').value;
     const ipSegment2 = document.getElementById('ipSegment2').value;
+    const ipSegment3 = document.getElementById('ipSegment3').value;
+    const ipSegment4 = document.getElementById('ipSegment4').value;
     const port = document.getElementById('port').value;
     const baseUrl = 'http://192.168.';
 
-    if (ipSegment1 && ipSegment2 && port) {
-      const url = `${baseUrl}${ipSegment1}.${ipSegment2}:${port}`;
+    if (ipSegment1 && ipSegment2 && ipSegment3 && ipSegment4 && port) {
+      const url = `http://${ipSegment1}.${ipSegment2}.${ipSegment3}.${ipSegment4}:${port}`;
 
       // Display the URL as a clickable link
       resultDiv.innerHTML = `<a href="${url}" target="_blank" class="text-blue-300 font-bold underline">${url}</a>`;
